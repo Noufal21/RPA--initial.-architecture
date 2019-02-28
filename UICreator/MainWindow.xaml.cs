c5f6985d35c0c65606b4344e7bc39f6460e3edf2
Muhammad Noufal <Noufal21@users.noreply.github.com>
2019-02-28 15:35:17 +0500

    msater

---
+++ UICreator/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UICreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void StackPanel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new Transaction());
+        }
+
+        private void StackPanel_MouseLeftButtonDown_History(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new History());
+        }
+
+        private void StackPanel_MouseLeftButtonDown_FLow(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new Flow());
+        }
+
+        private void StackPanel_MouseLeftButtonDown_Setings(object sender, MouseButtonEventArgs e)
+        {
+            MainFrame.Navigate(new Settings());
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+           // MainFrame.NavigationService.RemoveBackEntry();
+        }
+    }
+}
